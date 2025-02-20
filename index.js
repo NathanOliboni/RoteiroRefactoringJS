9803495ad91acba136eac86c541531e35e327cb4
NathanOliboni <nathan.oliboni@gmail.com>
2025-02-20 08:44:46 -0300

    Replace Temp with querry

--- index.js
+++ index.js
@@ -29,18 +29,21 @@ function gerarFaturaStr(fatura, pecas) {
         return total;
     }
 
+    function getPeca(apre) { // Função extraída
+        return pecas[apre.id];
+    }
+
     for (let apre of fatura.apresentacoes) {
-        const peca = pecas[apre.id];
-        let total = calcularTotalApresentacao(apre, peca); // valor da apresentação
+        let total = calcularTotalApresentacao(apre, getPeca(apre)); // valor da apresentação
 
         // créditos para próximas contratações
         creditos += Math.max(apre.audiencia - 30, 0);
-        if (peca.tipo === "comedia") {
+        if (getPeca(apre).tipo === "comedia") {
             creditos += Math.floor(apre.audiencia / 5);
         }
 
         // mais uma linha da fatura
-        faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+        faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
         totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
